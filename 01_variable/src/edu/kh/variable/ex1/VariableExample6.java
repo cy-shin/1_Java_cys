0e6ec4b0a7856274f4b7238fd65373db7e70dbe6
cymakp <110644369+cymakp@users.noreply.github.com>
2022-08-05 16:33:40 +0900

    220805-01 추가

--- 01_variable/src/edu/kh/variable/ex1/VariableExample6.java
+++ 01_variable/src/edu/kh/variable/ex1/VariableExample6.java
@@ -2,8 +2,13 @@ package edu.kh.variable.ex1;
 
 public class VariableExample6 {
 
+	// 메인 메서드
 	public static void main(String[] args) {
-
+		
+		int date = 5;
+		int month = 8;
+		
+		System.out.printf("오늘은 %02d월 %02d일입니다.",date,month);
 	}
 
 }
