1ff3aced2807ce8d6c3114706b274edfa1a94b70
cymakp <110644369+cymakp@users.noreply.github.com>
2022-08-07 15:06:02 +0900

    0807 연습
    push

---
+++ 01_variable/src/edu/kh/variable/ex1/BackUpPractice.java
@@ -0,0 +1,11 @@
+package edu.kh.variable.ex1;
+
+public class BackUpPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("안녕하세요");
+		System.out.println("오늘은 8월 7일 입니다.");
+	}
+
+}
