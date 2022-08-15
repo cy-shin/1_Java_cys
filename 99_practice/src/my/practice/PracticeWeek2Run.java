bd33841df525dff24fb549859969d1b7b49c3554
cymakp <110644369+cymakp@users.noreply.github.com>
2022-08-15 20:46:08 +0900

    0815 복습
    조건문 반복문 배열 기초 복습

---
+++ 99_practice/src/my/practice/PracticeWeek2Run.java
@@ -0,0 +1,15 @@
+package my.practice;
+
+public class PracticeWeek2Run {
+	public static void main(String[] args) {
+		PracticeWeek2 pr2 = new PracticeWeek2();
+		
+//		pr2.keyword();
+		
+//		pr2.condition();
+		
+//		pr2.loop();
+		
+		pr2.array();
+	}
+}
